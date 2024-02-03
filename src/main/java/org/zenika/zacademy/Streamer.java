2be705f4fc54510fa62758d937296cb96483cad1
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-02-03 11:51:41 +0100

    :sparkles: creating the class Streamers

---
+++ src/main/java/org/zenika/zacademy/Streamer.java
@@ -0,0 +1,16 @@
+package org.zenika.zacademy;
+
+public class Streamer {
+    public String name;
+    public int age;
+    public int streamMoney;
+    public String[] plateformes;
+
+
+    public Streamer(String name, int age, int streamMoney, String[] plateformes) {
+        this.name = name;
+        this.age = age;
+        this.streamMoney = streamMoney;
+        this.plateformes = plateformes;
+    }
+}
